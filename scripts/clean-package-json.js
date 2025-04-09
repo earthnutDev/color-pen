2c625cd173f77e067e82bf07948f5ef93415d332
earth nut dev <earthnut.dev@outlook.com>
2025-04-09 13:21:00 +0800

    en

--- scripts/clean-package-json.js
+++ scripts/clean-package-json.js
@@ -5,11 +5,60 @@ import {
   writeJsonFile,
 } from 'a-node-tools';
 
-const packageJson = readFileToJsonSync('./package.json');
+let packageJson = readFileToJsonSync('./package.json');
 
 ['scripts', 'devDependencies', 'lint-staged', 'private'].forEach(
   key => delete packageJson[key],
 );
+
+packageJson = {
+  ...packageJson,
+  main: 'cjs/index.cjs',
+  types: 'types/index.d.ts',
+  module: 'mjs/index.mjs',
+  files: ['mjs', 'cjs', 'types'],
+  exports: {
+    '.': {
+      import: {
+        default: './mjs/index.mjs',
+        types: './types/index.d.ts',
+      },
+      require: {
+        default: './cjs/index.cjs',
+        types: './types/index.d.ts',
+      },
+    },
+  },
+  keywords: ['color-pen'],
+  homepage: 'https://earthnut.dev/color-pen',
+  bugs: {
+    url: 'https://github.com/earthnutDev/color-pen/issues',
+    email: 'earthnut.dev@outlook.com',
+  },
+  repository: {
+    type: 'git',
+    url: 'git+https://github.com/earthnutDev/color-pen.git',
+  },
+  publishConfig: {
+    access: 'public',
+    registry: 'https://registry.npmjs.org/',
+  },
+  author: {
+    name: 'earthnut',
+    email: 'earthnut.dev@outlook.com',
+    url: 'https://earthnut.dev',
+  },
+  browserslist: [
+    'last 2 Chrome versions',
+    'last 2 Edge versions',
+    'not Safari >= 0',
+    'not IE >= 0',
+    'not Firefox >= 0',
+  ],
+  engines: {
+    node: '>=12.0.0',
+  },
+};
 
 const distPath = getDirectoryBy('dist', 'directory');
 
