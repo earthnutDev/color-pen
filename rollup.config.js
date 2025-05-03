871dcc16b1e8f281af60b52ab7ff579279708b3a
earth nut dev <earthnut.dev@outlook.com>
2025-05-03 19:13:47 +0800

    更新依赖个配置文件

--- rollup.config.js
+++ rollup.config.js
@@ -5,45 +5,7 @@ import json from '@rollup/plugin-json';
 // import terser from '@rollup/plugin-terser';
 import cleanup from 'rollup-plugin-cleanup';
 import copy from 'rollup-plugin-copy';
-import { readFileToJsonSync } from 'a-node-tools';
-import { pen } from 'color-pen';
-
-/** 配置需要不打包进生产包的包名配置  */
-const excludedPkg = ['node:', 'a-'];
-
-const excludedRegExp = new RegExp('^'.concat(excludedPkg.join('|^')));
-/**  读取当前文件  */
-const packInfo = readFileToJsonSync('./package.json');
-/**  已配置的依赖  */
-const dependencies = Object.keys({
-  ...(packInfo.dependencies || {}),
-  ...(packInfo.preDependencies || {}),
-});
-
-/**
- *
- * 依赖配置
- *
- */
-function external(id) {
-  excludedRegExp.lastIndex = 0;
-  const result = excludedRegExp.test(id);
-  /// 保证排除的包纯在于
-  if (result === true) {
-    if (dependencies.includes(id) === false) {
-      throw new RangeError(
-        `${pen.bgRed.blink.bold.yellow(id)} 依赖被排除打包却未再 package.json 中配置`,
-      );
-    }
-  } else {
-    if (/^[^./]/g.test(id)) {
-      throw new RangeError(
-        `${pen.bgRed.blink.bold.yellow(id)} 依赖未被排除，打包关闭`,
-      );
-    }
-  }
-  return result;
-}
+import { external } from '@qqi/rollup-external';
 
 export default {
   input: './index.ts',
@@ -67,7 +29,7 @@ export default {
     },
   ],
   // 配置需要排除的包
-  external,
+  external: external(),
   plugins: [
     resolve(),
     commonjs(),
